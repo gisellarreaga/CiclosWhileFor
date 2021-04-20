f59e4811a7b63c266349a4e8b1e552864d332f22
Gisell Arreaga <jackeline_gosell@hotmail.com>
2021-04-20 00:27:54 -0700

    Agregar archivos de proyecto.

---
+++ CiclosWhileFor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CiclosWhileFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            //For
+            //Parte 1 valor de la iteracion
+            //Parte2 condicion
+            //Parte 3 aumento o decremento
+
+            int tabla;
+            int i = 1; //parte 1
+
+            Console.WriteLine("Ingrese el número de la tabla que quiere calcular: ");
+            tabla = int.Parse(Console.ReadLine());
+
+            //Parte 2
+            while (i <= 10)
+            {
+                Console.WriteLine("" + tabla * i);
+                i++; //parte 3
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
